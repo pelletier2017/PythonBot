83a28c1bfb50e54da27a836a6f9eeef9c1ca40d8
ZERG3R <scmwhr83@gmail.com>
2018-02-17 18:23:26 -0500

    Delete whispers.py

--- whispers.py
+++
@@ -1,5 +0,0 @@
-def sendWhisper(s, user, message):
-    messageTemp = "PRIVMSG #jtv :.w" + user + " " + message
-    s.send(messageTemp + "\r\n")
-    print("Whisper: " +messageTemp)
-    sendWhisper(s, "zerg3rr", "testing")
