a95fb741a5f682c56a7b47ce107be95251568975
ZERG3R <scmwhr83@gmail.com>
2018-02-17 18:25:29 -0500

    Delete apiconnect.py

--- apiconnect.py
+++
@@ -1,8 +0,0 @@
-import requests
-import time
-import sys
-
-def main():
-
-    r_initial = requests.get('https://api.twitch.tv/kraken/channels/zerg3rr/follows')
-    current_follower_num = r_initial.json()['_total']
